75ab5f7358a0c208adbadff5a3a1ad4f50bd0101
Daniel Parucci Somerlate <89330408+Paruccikk@users.noreply.github.com>
2024-03-07 23:36:26 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-document.getElementById("playButton").addEventListener("click", function() {
-    var audioPlayer = document.getElementById("audioPlayer");
-    if (audioPlayer.paused) {
-        audioPlayer.play();
-        document.getElementById("playButton").innerText = "Pause";
-    } else {
-        audioPlayer.pause();
-        document.getElementById("playButton").innerText = "Play";
-    }
+var audio = document.getElementById('meuPlayer');
+var playPauseBtn = document.getElementById('playPauseBtn');
+
+playPauseBtn.addEventListener('click', function() {
+  if (audio.paused) {
+    audio.play();
+    playPauseBtn.innerHTML = 'Pause';
+  } else {
+    audio.pause();
+    playPauseBtn.innerHTML = 'Play';
+  }
 });
